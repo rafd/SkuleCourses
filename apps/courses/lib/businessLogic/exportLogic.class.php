411d026b3e75e913456681b017069a105441d114
wildabyss <wildabyss@gmail.com>
2009-08-17 15:17:53 -0400

    - fix security risk in index.php - csv export, untested

--- apps/courses/lib/businessLogic/exportLogic.class.php
+++ apps/courses/lib/businessLogic/exportLogic.class.php
@@ -2,9 +2,54 @@
 
 class exportLogic
 {
-  public function exportToCsv()
+  private $_dataArr;
+  
+  /**
+   * The param $dataArr must have the following structure:
+   * $dataArr = (1=>array(columnName=>value, columnName=>value...), 2=>array(...))
+   *
+   * @param       array $dataArr data array to be converted to csv
+   */
+  public function __construct($dataArr)
+  {
+    $this->_dataArr = $dataArr;
+  }
+  
+  public function addGraphics()
   {
     
+  }
+  
+  /**
+   * Exports $dataArr into csv as specified by the $fileName
+   *
+   * @param       string $fileName name, extension and path of the file to be exported
+   * @return      a javascript hook used for generating popup to get the csv file
+   */
+  public function exportToCsv($fileName)
+  {
+    // parse dataArr to csv
+    $csvOut = "";
+    foreach ($dataArr as $arr)
+    {
+      $firstInList = true;
+      foreach ($arr as $colName => $value)
+      {
+        if (!$firstInList) $csvOut.=',';
+        $firstInList = false;
+        
+        $csvOut .= '"'.$value.'"';
+      }
+      $csvOut .= "\n";
+    }
+    
+    // write to file
+    if (file_exists($fileName)) unlink($fileName);
+    $file = fopen($fileName, "w");
+    fwrite($file, $csvOut);
+    fclose($file);
+    
+    return "<script type='text/javascript'>window.open('".$fileName."', 'CSV Export')</script>";
   }
   
   public function exportToPdf()
--- www/index.php
+++ www/index.php
@@ -3,5 +3,5 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('courses', 'prod', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('courses', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
