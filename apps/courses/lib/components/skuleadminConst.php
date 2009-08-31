baa6893234f23e7b6fc8fa51fec9a3fd09169a1b
jason ko <jason.k.works@gmail.com>
2009-08-31 01:11:36 -0400

    added skuleadminConst.class.php

---
+++ apps/courses/lib/components/skuleadminConst.php
@@ -0,0 +1,13 @@
+<?php
+class skuleadminConst
+{
+  const DISCIPLINE_PARENT_ID = 100;
+  const SEM_SPRING = 1;
+  const SEM_WINTER = 9;
+  const SEM_SUMMER = 5;
+  const EXAMTYPE_PARENT_ID = 60;
+  const USERTYPE_PARENT_ID = 10;
+  const RATING_FIELD_PID = 22;
+  const BASE_UPLOAD_FOLDER = 'uploads';
+}
+?>
