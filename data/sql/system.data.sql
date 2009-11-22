ba881365554ba95196c2af5a8035526ff31a36e3
wildabyss <wildabyss@gmail.com>
2009-11-21 19:22:24 -0500

    data

--- data/sql/system.data.sql
+++ data/sql/system.data.sql
@@ -1,95 +1,152 @@
 # enum_item
 
-INSERT INTO `enum_item` VALUES (1, 1, '');
-INSERT INTO `enum_item` VALUES (10, 1, 'USER_TYPES');
-INSERT INTO `enum_item` VALUES (11, 10, 'Admin');
-INSERT INTO `enum_item` VALUES (12, 10, 'Moderator');
-INSERT INTO `enum_item` VALUES (13, 10, 'Guest');
-INSERT INTO `enum_item` VALUES (20, 1, 'META_RATING_TYPES');
-INSERT INTO `enum_item` VALUES (21, 20, 'Boolean');
-INSERT INTO `enum_item` VALUES (22, 20, 'Scale');
-INSERT INTO `enum_item` VALUES (23, 20, 'Number');
-INSERT INTO `enum_item` VALUES (30, 1, 'RATING_TYPES');
-INSERT INTO `enum_item` VALUES (31, 22, '3');
-INSERT INTO `enum_item` VALUES (32, 22, '4');
-INSERT INTO `enum_item` VALUES (33, 22, '5');
-INSERT INTO `enum_item` VALUES (34, 22, '6');
-INSERT INTO `enum_item` VALUES (35, 22, '7');
-INSERT INTO `enum_item` VALUES (36, 22, '8');
-INSERT INTO `enum_item` VALUES (60, 1, 'EXAM_TYPES');
-INSERT INTO `enum_item` VALUES (61, 60, 'Quiz');
-INSERT INTO `enum_item` VALUES (62, 60, 'Test');
-INSERT INTO `enum_item` VALUES (63, 60, 'Exam');
-INSERT INTO `enum_item` VALUES (100, 1, 'DISCIPLINES');
-INSERT INTO `enum_item` VALUES (101, 100, 'Chemical Engineering');
-INSERT INTO `enum_item` VALUES (102, 100, 'Electrical Engineering');
-INSERT INTO `enum_item` VALUES (103, 100, 'Civil Engineering');
-INSERT INTO `enum_item` VALUES (104, 100, 'Computer Engineering');
-INSERT INTO `enum_item` VALUES (105, 100, 'Engineering Science');
-INSERT INTO `enum_item` VALUES (106, 100, 'Engineering Science - Aerospace Option');
-INSERT INTO `enum_item` VALUES (107, 100, 'Engineering Science - Biomedical Option');
-INSERT INTO `enum_item` VALUES (108, 100, 'Engineering Science - Computer Option');
-INSERT INTO `enum_item` VALUES (109, 100, 'Engineering Science - Electrical Option');
-INSERT INTO `enum_item` VALUES (110, 100, 'Engineering Science - Energy Option');
-INSERT INTO `enum_item` VALUES (111, 100, 'Engineering Science - Infra Option');
-INSERT INTO `enum_item` VALUES (112, 100, 'Engineering Science - Manu Option');
-INSERT INTO `enum_item` VALUES (113, 100, 'Engineering Science - Nano Option');
-INSERT INTO `enum_item` VALUES (114, 100, 'Engineering Science - Physics Option');
-INSERT INTO `enum_item` VALUES (116, 100, 'Industrial Engineering');
-INSERT INTO `enum_item` VALUES (115, 100, 'Environemental Engineering');
-INSERT INTO `enum_item` VALUES (117, 100, 'Materials Engineering');
-INSERT INTO `enum_item` VALUES (118, 100, 'Mechanical Engineering');
-INSERT INTO `enum_item` VALUES (119, 100, 'Mineral Engineering');
-INSERT INTO `enum_item` VALUES (120, 100, 'Track One');
-INSERT INTO `enum_item` VALUES (121, 100, 'Unknown');
-INSERT INTO `enum_item` VALUES (200, 1, 'IMPORT_FILE_TYPES');
-INSERT INTO `enum_item` VALUES (201, 200, 'CSV');
-INSERT INTO `enum_item` VALUES (202, 200, 'XML');
-INSERT INTO `enum_item` VALUES (210, 1, 'EXPORT_FILE_TYPES');
-INSERT INTO `enum_item` VALUES (211, 210, 'CSV');
-INSERT INTO `enum_item` VALUES (212, 210, 'PDF');
-INSERT INTO `enum_item` VALUES (213, 210, 'XLS');
-INSERT INTO `enum_item` VALUES (220, 1, 'MAPPING_ITEMS');
-INSERT INTO `enum_item` VALUES (221, 220, 'Ignore');
-INSERT INTO `enum_item` VALUES (222, 220, 'Course Code');
-INSERT INTO `enum_item` VALUES (223, 220, 'Course Name');
-INSERT INTO `enum_item` VALUES (224, 220, 'Instructor Name');
-INSERT INTO `enum_item` VALUES (225, 220, 'Number Enrolled');
-INSERT INTO `enum_item` VALUES (226, 220, 'Number Response');
-INSERT INTO `enum_item` VALUES (227, 220, 'Question');
-INSERT INTO `enum_item` VALUES (228, 220, 'Question Mean');
-INSERT INTO `enum_item` VALUES (229, 220, 'Question Median');
-INSERT INTO `enum_item` VALUES (230, 220, 'Department Name');
+INSERT INTO `enum_item` (`id`,`parent_id`,`descr`) VALUES 
+ (1,1,''),
+ (10,1,'USER_TYPES'),
+ (11,10,'Admin'),
+ (12,10,'Moderator'),
+ (13,10,'Guest'),
+ (20,1,'META_RATING_TYPES'),
+ (21,20,'Boolean'),
+ (22,20,'Scale'),
+ (23,20,'Number'),
+ (30,1,'RATING_TYPES'),
+ (31,22,'3'),
+ (32,22,'4'),
+ (33,22,'5'),
+ (34,22,'6'),
+ (35,22,'7'),
+ (36,22,'8'),
+ (60,1,'EXAM_TYPES'),
+ (61,60,'Quiz'),
+ (62,60,'Test'),
+ (63,60,'Exam'),
+ (64,60,'Problem Set'),
+ (100,1,'DISCIPLINES'),
+ (101,100,'Chemical Engineering'),
+ (102,100,'Electrical Engineering'),
+ (103,100,'Civil Engineering'),
+ (104,100,'Computer Engineering'),
+ (105,100,'Engineering Science'),
+ (106,100,'Engineering Science - Aerospace Option'),
+ (107,100,'Engineering Science - Biomedical Option'),
+ (108,100,'Engineering Science - Computer Option'),
+ (109,100,'Engineering Science - Electrical Option'),
+ (110,100,'Engineering Science - Energy Option'),
+ (111,100,'Engineering Science - Infra Option'),
+ (112,100,'Engineering Science - Manu Option'),
+ (113,100,'Engineering Science - Nano Option'),
+ (114,100,'Engineering Science - Physics Option'),
+ (115,100,'Environemental Engineering'),
+ (116,100,'Industrial Engineering'),
+ (117,100,'Materials Engineering'),
+ (118,100,'Mechanical Engineering'),
+ (119,100,'Mineral Engineering'),
+ (120,100,'Track One'),
+ (121,100,'Unknown'),
+ (122,100,'Computer Engineering - Photonics & Semiconductor'),
+ (123,100,'Computer Engineering - EM & Energy Systems'),
+ (124,100,'Computer Engineering - Analog & Digital Electronics'),
+ (125,100,'Computer Engineering - Control, Comm & Signal Processing'),
+ (126,100,'Computer Engineering - Computer Hardware & Networks'),
+ (127,100,'Computer Engineering - Software'),
+ (128,100,'Electrical Engineering - Photonics & Semiconductor'),
+ (129,100,'Electrical Engineering - EM & Energy Systems'),
+ (130,100,'Electrical Engineering - Analog & Digital Electronics'),
+ (131,100,'Electrical Engineering - Control, Comm & Signal Processing'),
+ (132,100,'Electrical Engineering - Computer Hardware & Networks'),
+ (133,100,'Electrical Engineering - Software'),
+ (134,100,'Engineering Science - Electrical and Computer Option'),
+ (200,1,'IMPORT_FILE_TYPES'),
+ (201,200,'CSV'),
+ (202,200,'XML'),
+ (210,1,'EXPORT_FILE_TYPES'),
+ (211,210,'CSV'),
+ (212,210,'PDF'),
+ (213,210,'XLS'),
+ (220,1,'MAPPING_ITEMS'),
+ (221,220,'Ignore'),
+ (222,220,'Course Code'),
+ (223,220,'Course Name'),
+ (224,220,'Instructor Name'),
+ (225,220,'Number Enrolled'),
+ (226,220,'Number Response'),
+ (227,220,'Question'),
+ (228,220,'Question Mean'),
+ (229,220,'Question Median'),
+ (230,220,'Department Name');
 
 
 # department
 
-INSERT INTO `department` VALUES ('APM', 'Applied Mathematics');
-INSERT INTO `department` VALUES ('AER', 'Aerospace Science and Engineering');
-INSERT INTO `department` VALUES ('APS', 'Applied Science and Engineering');
-INSERT INTO `department` VALUES ('BME', 'Biomedical Engineering');
-INSERT INTO `department` VALUES ('CHE', 'Chemical Engineering and Applied Chemistry');
-INSERT INTO `department` VALUES ('CME', 'Civil and Mineral Engineering');
-INSERT INTO `department` VALUES ('CHM', 'Department of Chemistry');
-INSERT INTO `department` VALUES ('CIV', 'Department of Civil Engineering');
-INSERT INTO `department` VALUES ('CSC', 'Department of Computer Science');
-INSERT INTO `department` VALUES ('ECE', 'Department of Electrical and Comp Engineering');
-INSERT INTO `department` VALUES ('EDV', 'Division of Environmental Engineering and Energy Systems');
-INSERT INTO `department` VALUES ('EDC', 'EDC');
-INSERT INTO `department` VALUES ('EDM', 'EDM');
-INSERT INTO `department` VALUES ('ESC', 'Division of Engineering Science');
-INSERT INTO `department` VALUES ('GLG', 'Department of Geology');
-INSERT INTO `department` VALUES ('HPS', 'Institute for the History and Philosophy of Science & Technology');
-INSERT INTO `department` VALUES ('JGP', 'Joint');
-INSERT INTO `department` VALUES ('JVM', 'JVM');
-INSERT INTO `department` VALUES ('MAT', 'Department of Mathematics');
-INSERT INTO `department` VALUES ('MIE', 'Department of Mechanical and Industrial Engineering');
-INSERT INTO `department` VALUES ('MIN', 'Mineral Engineering');
-INSERT INTO `department` VALUES ('MMS', 'MMS');
-INSERT INTO `department` VALUES ('MSE', 'Department of Materials Science and Engineering');
-INSERT INTO `department` VALUES ('PHL', 'Department of Philosophy');
-INSERT INTO `department` VALUES ('PHY', 'Department of Physics');
-INSERT INTO `department` VALUES ('STA', 'Department of Statistics');
+INSERT INTO `department` (`id`,`descr`) VALUES 
+ ('AER','Aerospace Science and Engineering'),
+ ('ANA','Anatomy & Cell Biology'),
+ ('APM','Applied Mathematics'),
+ ('APS','Applied Science and Engineering'),
+ ('BME','Biomedical Engineering'),
+ ('ENV','Centre for Environment'),
+ ('CHE','Chemical Engineering and Applied Chemistry'),
+ ('CME','Civil and Mineral Engineering'),
+ ('ANT','Department of Anthropology'),
+ ('ARH','Department of Archaeology'),
+ ('ARC','Department of Architecture, Landscape and Design'),
+ ('AST','Department of Astronomy and Astrophysics'),
+ ('BCH','Department of Biochemistry'),
+ ('BCB','Department of Bioinformatics & Computational Biology'),
+ ('CHM','Department of Chemistry'),
+ ('CIV','Department of Civil Engineering'),
+ ('CLA','Department of Classical Studies'),
+ ('COL','Department of Comparative Literature'),
+ ('CSC','Department of Computer Science'),
+ ('EAS','Department of East Asian Studies'),
+ ('EEB','Department of Ecology & Evolutionary Biology'),
+ ('ECO','Department of Economics'),
+ ('ECE','Department of Electrical and Comp Engineering'),
+ ('ENG','Department of English'),
+ ('EUR','Department of European Studies'),
+ ('FAH','Department of Fine Art History'),
+ ('FOR','Department of Forest Conservation'),
+ ('FCS','Department of French Cultural Studies'),
+ ('FRE','Department of French Studies'),
+ ('GGR','Department of Geography'),
+ ('GLG','Department of Geology'),
+ ('HIS','Department of History'),
+ ('IMM','Department of Immunology'),
+ ('LIN','Department of Linguistics'),
+ ('MSE','Department of Materials Science and Engineering'),
+ ('MAT','Department of Mathematics'),
+ ('MIE','Department of Mechanical and Industrial Engineering'),
+ ('MMS','Department of Metallurgy and Materials Science'),
+ ('MGY','Department of Molecular Genetics & Microbiology'),
+ ('MUS','Department of Music'),
+ ('NRS','Department of Neuroscience'),
+ ('NFS','Department of Nutritional Sciences'),
+ ('PCL','Department of Pharmacology and Toxicology'),
+ ('PHL','Department of Philosophy'),
+ ('PHE','Department of Physical Education & Health'),
+ ('PHY','Department of Physics'),
+ ('PSL','Department of Physiology'),
+ ('PLN','Department of Planetary Science'),
+ ('POL','Department of Political Science'),
+ ('PSY','Department of Psychology'),
+ ('SOC','Department of Sociology'),
+ ('STA','Department of Statistics'),
+ ('ESC','Division of Engineering Science'),
+ ('EDC','Division of Environmental Engineering'),
+ ('EDM','Division of Environmental Engineering'),
+ ('EDV','Division of Environmental Engineering and Energy Systems'),
+ ('DRM','Drama Program'),
+ ('ELE','ELE'),
+ ('HMB','Human Biology'),
+ ('HPS','Institute for the History and Philosophy of Science & Technology'),
+ ('JSM','Joint Program'),
+ ('JTC','Joint Program'),
+ ('JVM','Joint Program'),
+ ('JXX','Joint Program'),
+ ('LMP','Laboratory Medicine & Pathobiology'),
+ ('MIN','Mineral Engineering'),
+ ('RLG','Religion');
 
 
 # rating_field
