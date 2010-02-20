33d738b3151c74be32c20407e5c158b242904ac2
wildabyss <wildabyss@gmail.com>
2010-02-20 18:03:28 -0500

    revert some of the cascade delete

--- data/sql/lib.model.schema.sql
+++ data/sql/lib.model.schema.sql
@@ -167,8 +167,7 @@ CREATE TABLE `course_rating_data`
 	INDEX `course_rating_data_FI_2` (`field_id`),
 	CONSTRAINT `course_rating_data_FK_2`
 		FOREIGN KEY (`field_id`)
-		REFERENCES `rating_field` (`id`)
-		ON DELETE CASCADE,
+		REFERENCES `rating_field` (`id`),
 	CONSTRAINT `course_rating_data_FK_3`
 		FOREIGN KEY (`course_ins_id`)
 		REFERENCES `course_instructor_assoc` (`id`)
@@ -195,8 +194,7 @@ CREATE TABLE `auto_course_rating_data`
 	KEY `auto_course_rating_data_I_2`(`course_ins_id`),
 	CONSTRAINT `auto_course_rating_data_FK_1`
 		FOREIGN KEY (`field_id`)
-		REFERENCES `rating_field` (`id`)
-		ON DELETE CASCADE,
+		REFERENCES `rating_field` (`id`),
 	CONSTRAINT `auto_course_rating_data_FK_2`
 		FOREIGN KEY (`course_ins_id`)
 		REFERENCES `course_instructor_assoc` (`id`)
@@ -329,7 +327,6 @@ CREATE TABLE `instructor`
 	CONSTRAINT `instructor_FK_1`
 		FOREIGN KEY (`dept_id`)
 		REFERENCES `department` (`id`)
-		ON DELETE CASCADE
 )Type=InnoDB;
 
 #-----------------------------------------------------------------------------
@@ -370,8 +367,7 @@ CREATE TABLE `import_mapping`
 	INDEX `import_mapping_FI_1` (`import_file_type`),
 	CONSTRAINT `import_mapping_FK_1`
 		FOREIGN KEY (`import_file_type`)
-		REFERENCES `enum_item` (`id`)
-		ON DELETE CASCADE,
+		REFERENCES `enum_item` (`id`),
 	INDEX `import_mapping_FI_2` (`mapping`),
 	CONSTRAINT `import_mapping_FK_2`
 		FOREIGN KEY (`mapping`)
@@ -402,7 +398,6 @@ CREATE TABLE `rating_field`
 	CONSTRAINT `rating_field_FK_1`
 		FOREIGN KEY (`type_id`)
 		REFERENCES `enum_item` (`id`)
-		ON DELETE CASCADE
 )Type=InnoDB;
 
 #-----------------------------------------------------------------------------
